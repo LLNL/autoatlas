a01ecea87725c336bef6f28797b77d4aa6ee9b51
Kadri Aditya Mohan <mohan3@pascal83.llnl.gov>
2019-09-11 18:18:40 -0700

    adding job submission scripts

---
+++ train_job.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -t 24:00:00
+#SBATCH -p pbatch
+#SBATCH -A ccp
+#SBATCH -D /usr/WS2/tbidata/workspace_aditya/Devs/autoseg
+##SBATCH --license=lscratche
+#SBATCH -o /usr/WS2/tbidata/workspace_aditya/Devs/autoseg/log.txt
+
+python demo.py
